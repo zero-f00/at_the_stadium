07b57905edc35b9528fd70de07fdb31aaa84b93d
yuuto.masamura <yuto.masamura@gmail.com>
2021-01-19 01:03:57 +0900

    素材画像とログイン、新規ユーザ画面のレイアウト作成まで

---
+++ AtTheStadium/CreateViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CreateViewController.swift
+//  AtTheStadium
+//
+//  Created by Yuto Masamura on 2021/01/19.
+//
+
+import UIKit
+
+class CreateViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
---
+++ AtTheStadium/LoginViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LoginViewController.swift
+//  AtTheStadium
+//
+//  Created by Yuto Masamura on 2021/01/19.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
